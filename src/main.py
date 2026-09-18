ab13c14af00df6c5900ea2f1414d7247450e4abf
JeauLabyorteaux <jeau711@gmail.com>
2026-09-17 18:33:21 -0700

    refactor and cleanup

--- src/main.py
+++ src/main.py
@@ -9,41 +9,36 @@ from dotenv import load_dotenv
 # Load variables in from env
 load_dotenv()
 
-# Key
-groq_key = os.getenv("GROQ_KEY")
-
 # Set up Groq client with api key
-groq_client = Groq(api_key=groq_key)
-
-# Setup for getting models
-models_url = "https://api.groq.com/openai/v1/models"
-
-models_headers = {
-    "Authorization":f"Bearer {groq_key}",
-    "Content-Type": "application/json",
-}
+GROQ_CLIENT = Groq(api_key=os.getenv("GROQ_KEY"))
+GROQ_MODEL = "openai/gpt-oss-20b"
 
 # Load character prompts
 curr_dir = os.path.dirname(os.path.abspath(__file__))
 
 prompts_file = os.path.join(curr_dir,'data', 'system_prompts.json')
 
-with open(prompts_file, 'r', encoding='utf-8') as file:
-    SYSTEM_CHARACTER_PROMPTS = json.load(file)
+try:
+    with open(prompts_file, 'r', encoding='utf-8') as file:
+        SYSTEM_CHARACTER_PROMPTS = json.load(file)
+except Exception as e:
+    print("Character profiles not found!")
+    print(e)
+    exit()
+
 
 
 def groq_request(system_character, user_query):
 
-    stream = groq_client.chat.completions.create(
-        model="openai/gpt-oss-20b",
+    stream = GROQ_CLIENT.chat.completions.create(
+        model=GROQ_MODEL,
         messages=[
-            {"role": "system", "content": SYSTEM_CHARACTER_PROMPTS[system_character]},
+            {"role": "system", "content": SYSTEM_CHARACTER_PROMPTS.get(system_character)},
             {"role": "user", "content": user_query}
         ],
-        temperature=0.5,                            
-        max_tokens=250,                           
-        seed=42,
-        stream=True,                           
+        temperature=0.8,                         
+        max_tokens=250,
+        stream=True,                   
     )
 
     full_response = ""
@@ -51,13 +46,9 @@ def groq_request(system_character, user_query):
     for chunk in stream:
         new_text = chunk.choices[0].delta.content or ""
         full_response += new_text
-        print(new_text, end="")
+        print(new_text, end="", flush=True)
 
     return full_response
-
-def get_models():
-    response = requests.get(models_url, headers=models_headers)
-    print(json.dumps(response.json(),indent=4))
 
 def main():
 
